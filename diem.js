ea2ad28b0282f5c7675280a0ec65459c01bcf23e
DuyLinh2003 <104605068+DuyLinh2003@users.noreply.github.com>
2022-05-21 20:13:29 +0700

    Create diem.js

---
+++ diem.js
@@ -0,0 +1,9 @@
+function kq(){
+    debugger;
+    let diemvl = Number(document.getElementById('number1').value);
+    let dienhh = Number(document.getElementById('number2').value);
+    let diemsh = Number(document.getElementById('number3').value);
+    document.getElementById('tong').value= diemvl + dienhh + diemsh;
+    let tb = (diemvl + dienhh + diemsh)/3;
+    document.getElementById('tbc').value= tb;  
+}
